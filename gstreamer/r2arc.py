513e28ca49467cc5bf6846d16400627ab1798e53
Reddimus <Kevi.M.CECS@outlook.com>
2024-04-05 20:32:23 -0700

    Added main file

---
+++ gstreamer/r2arc.py
@@ -0,0 +1,13 @@
+# import detect
+import ble
+
+# Define UUIDs for BLE service and characteristic
+# SERVICE_UUID = '12345678-1234-1234-1234-123456789012'
+# CHARACTERISTIC_UUID = '87654321-4321-4321-4321-210987654321'
+
+if __name__ == '__main__':
+    SERVICE_UUID = '12345678-1234-1234-1234-123456789012'
+    CHARACTERISTIC_UUID = '87654321-4321-4321-4321-210987654321'
+    r2ble = ble.R2ARC_Service(SERVICE_UUID, CHARACTERISTIC_UUID)
+    r2ble.start()
+    # detect.main()
