a6717cb67bc8e6797ddc2d5ac58f95fe94e29aa8
KeviM <Kevi.M.CECS@outlook.com>
2024-04-07 00:41:45 -0700

    Connected ble communication with r2arc

--- gstreamer/ble.py
+++ gstreamer/ble.py
@@ -1,10 +1,12 @@
+# ble.py
+# Bluetooth Low Energy (BLE) service for the R2ARC project.
+
 import os
-import threading
 import time
 import pybleno
 import array
 
-class SimpleCharacteristic(pybleno.Characteristic):
+class CustomCharacteristic(pybleno.Characteristic):
     """A custom characteristic for handling write requests."""
     def __init__(self, uuid):
         super().__init__({
@@ -18,50 +20,64 @@ class SimpleCharacteristic(pybleno.Characteristic):
         self._value = data  # Update the value with incoming data
         print(f'Received message: {self._value}, decoded: {self._value.decode("utf-8")}')
         callback(pybleno.Characteristic.RESULT_SUCCESS)
+        # print(self._value)
+        # Write to standard input console then send
+        # os.write(0, self._value)
+        # os.write(0, b'\n')
+
+    def getValue(self) -> str:
+        while not self._value:
+            pass
+        return self._value.decode('utf-8')
+    
+    def resetValue(self):
+        self._value = array.array('B', [0]*0)
 
 class R2ARC_Service:
     """A class to handle the BLE service and characteristic."""
     def __init__(self, service_uuid, characteristic_uuid):
         self._service_uuid = service_uuid
         self._characteristic_uuid = characteristic_uuid
+        self.characteristic = CustomCharacteristic(characteristic_uuid)
         self.reset_bluetooth()
         self._bleno = pybleno.Bleno()
+        self.ready = False
 
     def get_characteristic_uuid(self):
         return self._characteristic_uuid
     
     def get_service_uuid(self):
         return self._service_uuid
+    
+    def stop(self):
+        print("Stopping Pybleno")
+        self._bleno.stopAdvertising()
+        self._bleno.disconnect()
+        self._bleno = None
+        self.ready = False
 
     def reset_bluetooth(self, delay: float = 0.2) -> None:
         """Resets the Bluetooth service on the system."""
         print("Resetting Bluetooth")
         os.system('sudo systemctl stop bluetooth')
+        self.ready = False
         time.sleep(delay)  # Short pause
         os.system('sudo systemctl start bluetooth')
         time.sleep(delay)  # Wait for Bluetooth service to restart
+        self.ready = True
 
     def reset_ble_services(self):
         """
         Resets the Pybleno instance and restarts the Bluetooth service.
         Will throw an error; used for testing.
         """
-        print("Stopping Pybleno")
-        self._bleno.stopAdvertising()
-        self._bleno.disconnect()
-        self._bleno = None  # Clear the instance
+        self.stop()  # Stop the Pybleno instance
 
         self.reset_bluetooth()  # Restart the Bluetooth service
 
         print("Restarting Pybleno")
         self._bleno = pybleno.Bleno()  # Reinitialize Pybleno
-        self._setup_bleno()  # Setup Bleno with services and characteristics
-
-    def _setup_bleno(self):
-        """Sets up Bleno with event listeners and starts advertising."""
-        self._bleno.on('stateChange', self._onStateChange)
-        self._bleno.on('advertisingStart', self._onAdvertisingStart)
-        self._bleno.start()
+        self.setup()  # Setup Bleno with services and characteristics
 
     def _onStateChange(self, state):
         """Handles state changes in Bleno."""
@@ -78,18 +94,20 @@ class R2ARC_Service:
             self._bleno.setServices([
                 pybleno.BlenoPrimaryService({
                     'uuid': self._service_uuid,
-                    'characteristics': [SimpleCharacteristic(self._characteristic_uuid)]
+                    'characteristics': [self.characteristic]
                 })
             ])
+            self.ready = True
 
-    def start(self):
-        """Starts the BLE service."""
-        self._setup_bleno()
+    def setup(self):
+        """Sets up Bleno with event listeners and starts advertising."""
+        self._bleno.on('stateChange', self._onStateChange)
+        self._bleno.on('advertisingStart', self._onAdvertisingStart)
+        self._bleno.start()
+        while not self.ready:
+            pass
 
-        try:
-            while True:
-                pass
-        except KeyboardInterrupt:
-            # Graceful shutdown on keyboard interrupt
-            print("Program manually terminated")
-            self._bleno.stopAdvertising()
+    def update_user_input(self) -> str:
+        data = self.characteristic.getValue()
+        self.characteristic.resetValue()
+        return data
